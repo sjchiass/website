be4065ffaecba97c2aa5cf7ac9fc7902fd58fc18
Sebastien Chiasson <sjchiass@gmail.com>
2020-07-19 14:04:05 -0400

    Update site address/domain

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://138.197.159.218'
+SITEURL = 'https://sjchiass.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
