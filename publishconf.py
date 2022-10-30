d7b5599d84072bbe6390752bfca8b61261ca27cc
Sebastien Chiasson <sjchiass@gmail.com>
2022-10-30 17:34:34 -0400

    Again adjust config

--- publishconf.py
+++ publishconf.py
@@ -6,48 +6,18 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-AUTHOR = 'Sebastien Chiasson'
-SITENAME = 'sjchiass'
+# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://sjchiass.com'
-SITENAME = "Errata"
-SITETITLE = "Errata Blog"
-SITESUBTITLE = "Cats, hobbies, etc"
-SITEDESCRIPTION = "A blog mostly about electronics, cats and some coding"
 SITELOGO = SITEURL + "/images/logo.png"
 FAVICON = SITEURL + "/images/logo.png"
+RELATIVE_URLS = False
 
-PATH = 'content'
-THEME = "./Flex"
-THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-TIMEZONE = 'Canada/Eastern'
+DELETE_OUTPUT_DIRECTORY = True
 
-DEFAULT_LANG = 'en'
+# Following items are often useful when publishing
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Main Menu
-MAIN_MENU = True
-MENUITEMS = (("Archives", "/archives"), ("Categories", "/categories"), ("Tags", "/tags"),)
-
-# Blogroll
-LINKS = (("Live plant feed", "https://plants.sjchiass.com/"),)
-
-# Social widget
-SOCIAL = (("github", "https://www.github.com/sjchiass"),
-            ("youtube", "https://www.youtube.com/@sjchiass"),
-            ("twitter", "https://twitter.com/sjchiass"),
-            ("instagram", "https://www.instagram.com/sjchiass/"),
-            ("facebook", "https://www.facebook.com/sjchiass"),)
-
-PLUGINS = None
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
